403e012222ab3f54e1eb9293cfd79f412247daba
{kdh1123} <81676485+kdh1123@users.noreply.github.com>
2022-05-24 20:56:56 +0900

    [ADD] : drawble files
    2022-05-24

--- domain/src/main/java/kr/hs/dgsw/smartschool/domain/request/SignUpRequest.kt
+++ domain/src/main/java/kr/hs/dgsw/smartschool/domain/request/SignUpRequest.kt
@@ -1,5 +1,13 @@
 package kr.hs.dgsw.smartschool.domain.request
 
+import android.hardware.SensorAdditionalInfo
+
 data class SignUpRequest(
-    val id:String
+    val id:String,
+    val pw: String,
+    val name: String,
+    val phone: String,
+    val email:String,
+    val accountType:String,
+    val additionalInfo: SensorAdditionalInfo
 )
