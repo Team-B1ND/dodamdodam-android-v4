2f26953b0b0c47de2d266fb1cd20cc228f49bb38
snack655 <cmjcmj1230@gmail.com>
2022-05-24 19:57:55 +0900

    Modify : ViewPager transform
    2022-05-24

--- presentation/src/main/java/kr/hs/dgsw/smartschool/dodamdodam/features/home/HomeFragment.kt
+++ presentation/src/main/java/kr/hs/dgsw/smartschool/dodamdodam/features/home/HomeFragment.kt
@@ -1,6 +1,10 @@
 package kr.hs.dgsw.smartschool.dodamdodam.features.home
 
+import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.viewpager2.widget.CompositePageTransformer
+import androidx.viewpager2.widget.MarginPageTransformer
+import androidx.viewpager2.widget.ViewPager2
 import kr.hs.dgsw.smartschool.dodamdodam.adapter.MealHomeAdapter
 import kr.hs.dgsw.smartschool.dodamdodam.base.BaseFragment
 import kr.hs.dgsw.smartschool.dodamdodam.databinding.FragmentHomeBinding
@@ -12,6 +16,20 @@ class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>() {
     override fun observerViewModel() {
         val mealHomeAdapter = MealHomeAdapter()
         mBinding.viewPagerMealList.adapter = mealHomeAdapter
+        mBinding.viewPagerMealList.offscreenPageLimit = 3
+        mBinding.viewPagerMealList.setPadding(90, 0, 90, 0)
+
+        var transform = CompositePageTransformer()
+        transform.addTransformer(MarginPageTransformer(10))
+
+        transform.addTransformer { view: View, fl: Float ->
+            var v = 1 - Math.abs(fl)
+            view.scaleY = 0.8f + v * 0.2f
+        }
+
+        mBinding.viewPagerMealList.setPageTransformer(transform)
+
+
         mealHomeAdapter.submitList(
             listOf(
                 MealInfo(1, "쇠고기버섯죽 , *크로크무슈 , 나박물김치 , *오레오오즈레드+우유 , 바나나"),
