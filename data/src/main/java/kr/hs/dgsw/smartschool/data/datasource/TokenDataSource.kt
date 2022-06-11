4f7f0abc9f7da1f1ae3b0fdca399d679c212782e
snack655 <cmjcmj1230@gmail.com>
2022-06-11 20:08:42 +0900

    Add : Token mapper
    2022-06-11

--- data/src/main/java/kr/hs/dgsw/smartschool/data/datasource/TokenDataSource.kt
+++ data/src/main/java/kr/hs/dgsw/smartschool/data/datasource/TokenDataSource.kt
@@ -2,12 +2,22 @@ package kr.hs.dgsw.smartschool.data.datasource
 
 import kr.hs.dgsw.smartschool.data.base.BaseDataSource
 import kr.hs.dgsw.smartschool.data.database.cache.TokenCache
+import kr.hs.dgsw.smartschool.data.mapper.TokenMapper
 import kr.hs.dgsw.smartschool.data.network.remote.TokenRemote
+import kr.hs.dgsw.smartschool.domain.model.token.Token
 import javax.inject.Inject
 
 class TokenDataSource @Inject constructor(
     override val remote: TokenRemote,
     override val cache: TokenCache
 ) : BaseDataSource<TokenRemote, TokenCache>() {
+
+    private val tokenMapper = TokenMapper()
+
+    private val PAYLOAD_MEMBER_ID = "memberId"
+
+    suspend fun insertToken(token: Token) = cache.insertToken(tokenMapper.mapToEntity(token))
+
+    
 
 }
