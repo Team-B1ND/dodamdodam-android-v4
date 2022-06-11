54e41e20966ba86a83d1c88b11fdfa4bab47c9a8
snack655 <cmjcmj1230@gmail.com>
2022-06-11 20:39:41 +0900

    Add : Token Data Source

--- data/src/main/java/kr/hs/dgsw/smartschool/data/datasource/TokenDataSource.kt
+++ data/src/main/java/kr/hs/dgsw/smartschool/data/datasource/TokenDataSource.kt
@@ -1,10 +1,16 @@
 package kr.hs.dgsw.smartschool.data.datasource
 
+import android.util.Base64
 import kr.hs.dgsw.smartschool.data.base.BaseDataSource
 import kr.hs.dgsw.smartschool.data.database.cache.TokenCache
+import kr.hs.dgsw.smartschool.data.database.entity.TokenEntity
 import kr.hs.dgsw.smartschool.data.mapper.TokenMapper
 import kr.hs.dgsw.smartschool.data.network.remote.TokenRemote
 import kr.hs.dgsw.smartschool.domain.model.token.Token
+import kr.hs.dgsw.smartschool.domain.request.TokenRequest
+import org.json.JSONException
+import org.json.JSONObject
+import java.util.*
 import javax.inject.Inject
 
 class TokenDataSource @Inject constructor(
@@ -18,6 +24,35 @@ class TokenDataSource @Inject constructor(
 
     suspend fun insertToken(token: Token) = cache.insertToken(tokenMapper.mapToEntity(token))
 
-    
+    suspend fun getToken(): TokenEntity = cache.getToken()
 
+    suspend fun updateNewToken(): TokenEntity =
+        insertNewToken(getToken().let { tokenEntity -> tokenMapper.mapToModel(tokenEntity) })
+
+    suspend fun deleteToken() = cache.deleteToken()
+
+    suspend fun getMyId(): String = getId(tokenMapper.mapToModel(getToken()))
+
+    private suspend fun insertNewToken(token: Token): TokenEntity =
+        remote.getNewToken(TokenRequest(token.refreshToken)).data
+            .let { newToken -> TokenEntity(newToken, token.refreshToken) }
+            .also { cache.insertToken(it) }
+
+    private fun getId(token: Token): String {
+        return try {
+            val payload = decodedPayloadObject(token.token)
+            payload!!.getString(PAYLOAD_MEMBER_ID)
+        } catch(ignore: JSONException) {
+            throw Throwable("아이디 오류")
+        }
+    }
+
+    private fun decodedPayloadObject(tokenString: String): JSONObject? {
+        return try {
+            val split = tokenString.split(".").toTypedArray()
+            JSONObject(String(Base64.decode(split[1], Base64.DEFAULT)))
+        } catch (ignore: JSONException) {
+            null
+        }
+    }
 }
