1d9459ba94b4e33b2e6d0bc4a4bbe62a760b347b
snack655 <cmjcmj1230@gmail.com>
2022-06-11 20:50:53 +0900

    Add : Account Data Source

--- data/src/main/java/kr/hs/dgsw/smartschool/data/datasource/AccountDataSource.kt
+++ data/src/main/java/kr/hs/dgsw/smartschool/data/datasource/AccountDataSource.kt
@@ -2,6 +2,7 @@ package kr.hs.dgsw.smartschool.data.datasource
 
 import kr.hs.dgsw.smartschool.data.base.BaseDataSource
 import kr.hs.dgsw.smartschool.data.database.cache.AccountCache
+import kr.hs.dgsw.smartschool.data.database.entity.AccountEntity
 import javax.inject.Inject
 
 class AccountDataSource @Inject constructor(
@@ -9,6 +10,9 @@ class AccountDataSource @Inject constructor(
     override val cache: AccountCache
 ) : BaseDataSource<Any, AccountCache>() {
 
-    suspend fun getAccount()
+    suspend fun getAccount(): AccountEntity = cache.getAccount()
 
+    suspend fun insertAccount(accountEntity: AccountEntity) = cache.insertAccount(accountEntity)
+
+    suspend fun deleteAccount() = cache.deleteAccount()
 }
