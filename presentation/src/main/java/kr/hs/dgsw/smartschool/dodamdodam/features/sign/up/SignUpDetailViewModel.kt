35c195fc6a6bfc9c1f6464c853296e422c5d2eb9
snack655 <cmjcmj1230@gmail.com>
2022-07-14 10:46:47 +0900

    Add : Font

--- presentation/src/main/java/kr/hs/dgsw/smartschool/dodamdodam/features/sign/up/SignUpDetailViewModel.kt
+++ presentation/src/main/java/kr/hs/dgsw/smartschool/dodamdodam/features/sign/up/SignUpDetailViewModel.kt
@@ -1,6 +1,5 @@
 package kr.hs.dgsw.smartschool.dodamdodam.features.sign.up
 
-import android.content.Intent
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
