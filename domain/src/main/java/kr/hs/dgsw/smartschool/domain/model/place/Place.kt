e019e3c845a3db8187309d63222bac2d777a684e
snack655 <cmjcmj1230@gmail.com>
2022-07-24 15:09:42 +0900

    Add : Location api

---
+++ domain/src/main/java/kr/hs/dgsw/smartschool/domain/model/place/Place.kt
@@ -0,0 +1,12 @@
+package kr.hs.dgsw.smartschool.domain.model.place
+
+data class Place(
+    val idx: Int,
+    val name: String
+) {
+    constructor(): this(0, "")
+
+    override fun toString(): String {
+        return name
+    }
+}
