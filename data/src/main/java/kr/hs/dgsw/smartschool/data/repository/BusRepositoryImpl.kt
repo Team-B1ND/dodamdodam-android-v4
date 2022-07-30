77b5f795a212ca54e77de290fe6f0d0c36291c3c
김도현 <81676485+kdh1123@users.noreply.github.com>
2022-07-30 17:09:32 +0900

    Update : BusRepository
    2022-07-30

--- data/src/main/java/kr/hs/dgsw/smartschool/data/repository/BusRepositoryImpl.kt
+++ data/src/main/java/kr/hs/dgsw/smartschool/data/repository/BusRepositoryImpl.kt
@@ -4,7 +4,10 @@ import kr.hs.dgsw.smartschool.data.datasource.BusDataSource
 import kr.hs.dgsw.smartschool.domain.model.bus.Bus
 import kr.hs.dgsw.smartschool.domain.model.bus.BusByDate
 import kr.hs.dgsw.smartschool.domain.repository.BusRepository
+import kr.hs.dgsw.smartschool.domain.request.AddBusRequest
 import kr.hs.dgsw.smartschool.domain.request.MyBusByMonthRequest
+import kr.hs.dgsw.smartschool.domain.request.UpdateBusApplyRequest
+import kr.hs.dgsw.smartschool.domain.request.UpdateBusRequest
 import javax.inject.Inject
 
 class BusRepositoryImpl @Inject constructor(
@@ -25,27 +28,29 @@ class BusRepositoryImpl @Inject constructor(
         return dataSource.getMyBusByMonth(request).busList
     }
 
-    override suspend fun addBus():String {
-        
-    }
+    override suspend fun addBus(request: AddBusRequest)
+    = dataSource.addBus(request)
 
-    override suspend fun addBusApply():String {
-        TODO("Not yet implemented")
-    }
+    override suspend fun addBusApply()
+    = dataSource.addBusApply()
 
-    override suspend fun updateBusInfo() :String{
-        TODO("Not yet implemented")
-    }
+    override suspend fun updateBus(
+        request: UpdateBusRequest
+    )
+    = dataSource.updateBus(request)
 
-    override suspend fun updateBusApply() :String{
-        TODO("Not yet implemented")
-    }
+    override suspend fun updateBusApply(
+        request: UpdateBusApplyRequest
+    )
+    =dataSource.updateBusApply(request)
 
-    override suspend fun deleteBus() :String{
-        TODO("Not yet implemented")
-    }
+    override suspend fun deleteBus(
+        idx : Int
+    )
+    = dataSource.deleteBus(idx)
 
-    override suspend fun deleteBusApply():String{
-        TODO("Not yet implemented")
-    }
+    override suspend fun deleteBusApply(
+        idx : Int
+    )
+    = dataSource.deleteBusApply(idx)
 }
--- domain/src/main/java/kr/hs/dgsw/smartschool/domain/repository/BusRepository.kt
+++ domain/src/main/java/kr/hs/dgsw/smartschool/domain/repository/BusRepository.kt
@@ -2,27 +2,43 @@ package kr.hs.dgsw.smartschool.domain.repository
 
 import kr.hs.dgsw.smartschool.domain.model.bus.Bus
 import kr.hs.dgsw.smartschool.domain.model.bus.BusByDate
+import kr.hs.dgsw.smartschool.domain.request.AddBusRequest
+import kr.hs.dgsw.smartschool.domain.request.MyBusByMonthRequest
+import kr.hs.dgsw.smartschool.domain.request.UpdateBusApplyRequest
+import kr.hs.dgsw.smartschool.domain.request.UpdateBusRequest
 
 interface BusRepository {
 
     //GET
     suspend fun getBusList(): List<BusByDate>
     suspend fun getMyBus() : List<Bus>
-    suspend fun getMyBusByMonth() : List<Bus>
+    suspend fun getMyBusByMonth(
+        request : MyBusByMonthRequest
+    ) : List<Bus>
 
 
     //POST
-    suspend fun addBus():String
-    suspend fun addBusApply():String
+    suspend fun addBus(
+        request: AddBusRequest
+    )
+    suspend fun addBusApply()
 
 
     //PUT
-    suspend fun updateBusInfo():String
-    suspend fun updateBusApply():String
+    suspend fun updateBus(
+        request: UpdateBusRequest
+    )
+    suspend fun updateBusApply(
+        request: UpdateBusApplyRequest
+    )
 
 
 
     //DELETE
-    suspend fun deleteBus():String
-    suspend fun deleteBusApply():String
+    suspend fun deleteBus(
+        idx : Int
+    )
+    suspend fun deleteBusApply(
+        idx : Int
+    )
 }
