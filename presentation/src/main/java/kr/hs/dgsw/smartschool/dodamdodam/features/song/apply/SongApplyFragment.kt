124af69c2cc70ace71b00906a560f3e4c564fd1f
snack655 <cmjcmj1230@gmail.com>
2022-08-11 16:32:05 +0900

    Add : 기상송 신청 기능 완성

--- presentation/src/main/java/kr/hs/dgsw/smartschool/dodamdodam/features/song/apply/SongApplyFragment.kt
+++ presentation/src/main/java/kr/hs/dgsw/smartschool/dodamdodam/features/song/apply/SongApplyFragment.kt
@@ -5,6 +5,7 @@ import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
 import kr.hs.dgsw.smartschool.dodamdodam.base.BaseFragment
 import kr.hs.dgsw.smartschool.dodamdodam.databinding.FragmentSongApplyBinding
+import kr.hs.dgsw.smartschool.dodamdodam.widget.extension.shortToast
 
 @AndroidEntryPoint
 class SongApplyFragment : BaseFragment<FragmentSongApplyBinding, SongApplyViewModel>() {
@@ -13,8 +14,19 @@ class SongApplyFragment : BaseFragment<FragmentSongApplyBinding, SongApplyViewMo
     override fun observerViewModel() {
         bindingViewEvent { event ->
             when (event) {
-                SongApplyViewModel.EVENT_ON_CLICK_BACK -> findNavController().popBackStack()
+                SongApplyViewModel.EVENT_ON_CLICK_BACK -> popBackStack()
+                SongApplyViewModel.EVENT_ON_URL_ERROR -> shortToast(viewModel.errorMessage)
+                SongApplyViewModel.EVENT_ON_SUCCESS_APPLY -> successApplySong()
             }
         }
+    }
+
+    private fun successApplySong() {
+        shortToast("기상송 신청에 성공했습니다.")
+        popBackStack()
+    }
+
+    private fun popBackStack() {
+        findNavController().popBackStack()
     }
 }
--- presentation/src/main/java/kr/hs/dgsw/smartschool/dodamdodam/features/song/apply/SongApplyViewModel.kt
+++ presentation/src/main/java/kr/hs/dgsw/smartschool/dodamdodam/features/song/apply/SongApplyViewModel.kt
@@ -1,6 +1,9 @@
 package kr.hs.dgsw.smartschool.dodamdodam.features.song.apply
 
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.launchIn
 import kr.hs.dgsw.smartschool.dodamdodam.base.BaseViewModel
 import kr.hs.dgsw.smartschool.domain.usecase.song.SongUseCases
 import javax.inject.Inject
@@ -10,11 +13,59 @@ class SongApplyViewModel @Inject constructor(
     private val songUseCases: SongUseCases
 ) : BaseViewModel() {
 
+    val applyUrl = MutableLiveData<String>()
+    var errorMessage = ""
+
+    private val isApplySongLoading = MutableLiveData(false)
+
+    init {
+        combineLoadingVariable(isApplySongLoading)
+    }
+
     fun onClickBack() {
         viewEvent(EVENT_ON_CLICK_BACK)
     }
 
+    fun checkUrl() {
+        //case 1) https://www.youtube.com/watch?v=Tn25KXWIeqQ
+        //case 2) https://youtu.be/o_nxIQTM_B0
+        val youtube = "https://www.youtube.com/watch?v="
+        val mYoutube = "https://youtu.be/"
+
+        when {
+            applyUrl.value.isNullOrBlank() -> {
+                errorMessage = "URL을 입력해주세요!"
+                viewEvent(EVENT_ON_URL_ERROR)
+                return
+            }
+            applyUrl.value?.startsWith(youtube) == true -> {
+                applyWakeUpSong(applyUrl.value ?: "")
+            }
+            applyUrl.value?.startsWith(mYoutube) == true -> {
+                val youtubeUrl = "https://www.youtube.com/watch?v=" + applyUrl.value?.replace(mYoutube, "")
+                applyWakeUpSong(youtubeUrl)
+            }
+            else -> {
+                errorMessage = "옳바른 형식의 URL을 입력해주세요!"
+                viewEvent(EVENT_ON_URL_ERROR)
+            }
+        }
+    }
+
+    private fun applyWakeUpSong(url: String) {
+        songUseCases.postSong(url).divideResult(
+            isApplySongLoading,
+            { viewEvent(EVENT_ON_SUCCESS_APPLY) },
+            {
+                errorMessage = it ?: "기상송 신청에 실패했습니다."
+                viewEvent(EVENT_ON_URL_ERROR)
+            }
+        ).launchIn(viewModelScope)
+    }
+
     companion object {
         const val EVENT_ON_CLICK_BACK = 0
+        const val EVENT_ON_URL_ERROR = 1
+        const val EVENT_ON_SUCCESS_APPLY = 2
     }
 }
