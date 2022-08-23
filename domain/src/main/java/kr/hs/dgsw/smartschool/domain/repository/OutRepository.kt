40d9797511d8ac10426e176b253a227ec1a268f1
최민재 <cmjcmj1230@gmail.com>
2022-08-23 19:33:36 +0900

    Add: DataSource and Repository

---
+++ domain/src/main/java/kr/hs/dgsw/smartschool/domain/repository/OutRepository.kt
@@ -0,0 +1,30 @@
+package kr.hs.dgsw.smartschool.domain.repository
+
+import kr.hs.dgsw.smartschool.domain.model.out.OutGoing
+import kr.hs.dgsw.smartschool.domain.model.out.OutItem
+import kr.hs.dgsw.smartschool.domain.model.out.OutSleeping
+import kr.hs.dgsw.smartschool.domain.request.OutRequest
+
+interface OutRepository {
+
+    suspend fun getOut(date: String): List<OutItem>
+
+    suspend fun getOutAllows(date: String): List<OutItem>
+
+    suspend fun getOutSleepingById(outSleepingIdx: Int): OutSleeping
+
+    suspend fun getOutGoingById(outGoingIdx: Int): OutGoing
+
+    suspend fun postOutSleeping(request: OutRequest): String
+
+    suspend fun putOutSleeping(request: OutRequest): String
+
+    suspend fun deleteOutSleeping(outSleepingIdx: Int): String
+
+    suspend fun postOutGoing(request: OutRequest): String
+
+    suspend fun putOutGoing(request: OutRequest): String
+
+    suspend fun deleteOutGoing(outGoingIdx: Int): String
+
+}
