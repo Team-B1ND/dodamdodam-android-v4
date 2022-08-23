19873ed26c1a38b0755aac972912f223d2d39dad
최민재 <cmjcmj1230@gmail.com>
2022-08-23 22:21:56 +0900

    Add: out date

--- presentation/src/main/java/kr/hs/dgsw/smartschool/dodamdodam/features/out/write/OutWriteFragment.kt
+++ presentation/src/main/java/kr/hs/dgsw/smartschool/dodamdodam/features/out/write/OutWriteFragment.kt
@@ -1,9 +1,14 @@
 package kr.hs.dgsw.smartschool.dodamdodam.features.out.write
 
+import android.app.DatePickerDialog
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import kr.hs.dgsw.smartschool.dodamdodam.base.BaseFragment
 import kr.hs.dgsw.smartschool.dodamdodam.databinding.FragmentOutWriteBinding
+import kr.hs.dgsw.smartschool.dodamdodam.widget.extension.dateFormat
+import kr.hs.dgsw.smartschool.dodamdodam.widget.extension.monthFormat
+import kr.hs.dgsw.smartschool.dodamdodam.widget.extension.yearFormat
+import java.util.*
 
 class OutWriteFragment : BaseFragment<FragmentOutWriteBinding, OutWriteViewModel>() {
     override val viewModel: OutWriteViewModel by viewModels()
@@ -13,7 +18,31 @@ class OutWriteFragment : BaseFragment<FragmentOutWriteBinding, OutWriteViewModel
         bindingViewEvent { event ->
             when (event) {
                 OutWriteViewModel.EVENT_ON_CLICK_BACK -> findNavController().popBackStack()
+                OutWriteViewModel.EVENT_ON_CLICK_START_OUT_GOING_DATE -> openDatePicker(0)
+                OutWriteViewModel.EVENT_ON_CLICK_START_OUT_SLEEPING_DATE -> openDatePicker(1)
+                OutWriteViewModel.EVENT_ON_CLICK_END_OUT_SLEEPING_DATE -> openDatePicker(2)
             }
+        }
+    }
+
+    private fun openDatePicker(mode: Int) {
+        val targetDate = when (mode) {
+            0 -> viewModel.startOutGoingDate
+            1 -> viewModel.startOutSleepingDate
+            2 -> viewModel.endOutSleepingDate
+            else -> return
+        }
+
+        targetDate.value?.apply {
+            val year = yearFormat().toInt()
+            val month = monthFormat().toInt() - 1
+            val date = dateFormat().toInt()
+
+            DatePickerDialog(this@OutWriteFragment.requireContext(), { _, y, m, d ->
+                val cal = Calendar.getInstance()
+                cal.set(y, m, d)
+                targetDate.value = cal.time
+            }, year, month, date).show()
         }
     }
 }
--- presentation/src/main/java/kr/hs/dgsw/smartschool/dodamdodam/features/out/write/OutWriteViewModel.kt
+++ presentation/src/main/java/kr/hs/dgsw/smartschool/dodamdodam/features/out/write/OutWriteViewModel.kt
@@ -2,11 +2,18 @@ package kr.hs.dgsw.smartschool.dodamdodam.features.out.write
 
 import androidx.lifecycle.MutableLiveData
 import kr.hs.dgsw.smartschool.dodamdodam.base.BaseViewModel
+import java.util.*
 
 class OutWriteViewModel : BaseViewModel() {
 
     val isOutGoing = MutableLiveData(true)
     val isOutSleeping = MutableLiveData(false)
+
+    val startOutSleepingDate = MutableLiveData(Date())
+    val endOutSleepingDate = MutableLiveData(Date())
+    val startOutGoingDate = MutableLiveData(Date())
+
+    val outReason = MutableLiveData<String>()
 
     fun onClickBack() {
         viewEvent(EVENT_ON_CLICK_BACK)
@@ -22,7 +29,20 @@ class OutWriteViewModel : BaseViewModel() {
         isOutSleeping.value = true
     }
 
+    fun onClickStartOutGoingDate() {
+        viewEvent(EVENT_ON_CLICK_START_OUT_GOING_DATE)
+    }
+    fun onClickStartOutSleepingDate() {
+        viewEvent(EVENT_ON_CLICK_START_OUT_SLEEPING_DATE)
+    }
+    fun onClickEndOutSleepingDate() {
+        viewEvent(EVENT_ON_CLICK_END_OUT_SLEEPING_DATE)
+    }
+
     companion object {
         const val EVENT_ON_CLICK_BACK = 0
+        const val EVENT_ON_CLICK_START_OUT_GOING_DATE = 1
+        const val EVENT_ON_CLICK_START_OUT_SLEEPING_DATE = 2
+        const val EVENT_ON_CLICK_END_OUT_SLEEPING_DATE = 3
     }
 }
