e4a52269fc4434dac178a6757e9ff334f31b10af
최민재 <cmjcmj1230@gmail.com>
2022-08-28 18:16:31 +0900

    Add : v6 Data source

--- data/src/main/java/kr/hs/dgsw/smartschool/data/datasource/TimeTableDataSource.kt
+++ data/src/main/java/kr/hs/dgsw/smartschool/data/datasource/TimeTableDataSource.kt
@@ -1,33 +1,33 @@
 package kr.hs.dgsw.smartschool.data.datasource
 
 import kr.hs.dgsw.smartschool.data.base.BaseDataSource
-import kr.hs.dgsw.smartschool.data.database.cache.TimeCache
+import kr.hs.dgsw.smartschool.data.database.cache.TimeTableCache
 import kr.hs.dgsw.smartschool.data.database.entity.TimeEntity
-import kr.hs.dgsw.smartschool.data.mapper.TimeMapper
-import kr.hs.dgsw.smartschool.data.network.remote.TimeRemote
+import kr.hs.dgsw.smartschool.data.mapper.TimeTableMapper
+import kr.hs.dgsw.smartschool.data.network.remote.TimeTableRemote
 import kr.hs.dgsw.smartschool.domain.model.time.TimeTable
 import kr.hs.dgsw.smartschool.domain.model.time.WeekType
 import kr.hs.dgsw.smartschool.domain.util.Utils
 import javax.inject.Inject
 
-class TimeDataSource @Inject constructor(
-    override val remote: TimeRemote,
-    override val cache: TimeCache
-) : BaseDataSource<TimeRemote, TimeCache> {
+class TimeTableDataSource @Inject constructor(
+    override val remote: TimeTableRemote,
+    override val cache: TimeTableCache
+) : BaseDataSource<TimeTableRemote, TimeTableCache> {
 
-    private val timeMapper = TimeMapper()
+    private val timeTableMapper = TimeTableMapper()
 
     suspend fun updateTimeList() =
         cache.deleteAll().also { insertAllTimeInfoRemote() }
 
     suspend fun getAllTime(): List<TimeEntity> =
-        if (Utils.getWeekType() == WeekType.DAY)
+        if (Utils.getWeekType() == WeekType.WEEKDAY)
             cache.getAllWeekdayTime().also { if (it.isEmpty()) getTimeRemote() }
         else
             cache.getAllWeekendTime().also { if (it.isEmpty()) getTimeRemote() }
 
     suspend fun getAllTime(dayOfWeek: Int): List<TimeEntity> =
-        if (Utils.getWeekType(dayOfWeek) == WeekType.DAY)
+        if (Utils.getWeekType(dayOfWeek) == WeekType.WEEKDAY)
             cache.getAllWeekendTime().also { if (it.isEmpty()) getTimeRemote() }
         else
             cache.getAllWeekendTime().also { if (it.isEmpty()) getTimeRemote() }
@@ -36,9 +36,9 @@ class TimeDataSource @Inject constructor(
         remote.getAllTime().also { insertTimeList(it) }
 
     private suspend fun getTimeRemote(): List<TimeEntity> =
-        remote.getAllTime().map { time -> timeMapper.mapToEntity(time) }
+        remote.getAllTime().map { time -> timeTableMapper.mapToEntity(time) }
             .also { timeEntityList -> cache.insertTime(timeEntityList) }
 
     private suspend fun insertTimeList(timeTableList: List<TimeTable>) =
-        cache.insertTime(timeTableList.map { time -> timeMapper.mapToEntity(time) })
+        cache.insertTime(timeTableList.map { time -> timeTableMapper.mapToEntity(time) })
 }
--- data/src/main/java/kr/hs/dgsw/smartschool/data/network/remote/TimeRemote.kt
+++
@@ -1,9 +0,0 @@
-package kr.hs.dgsw.smartschool.data.network.remote
-
-import kr.hs.dgsw.smartschool.data.base.remote.BaseRemote
-import kr.hs.dgsw.smartschool.data.network.api.TimeApi
-import kr.hs.dgsw.smartschool.domain.model.time.TimeTable
-
-class TimeRemote(override val api: TimeApi) : BaseRemote<TimeApi>() {
-    suspend fun getAllTime(): List<TimeTable> = api.getTimeTable().data.timeTables
-}
