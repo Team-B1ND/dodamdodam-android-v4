5e499e9ece482d84c5c66113840c2d3dccb502c1
DoHyeon <kdh200533@gmail.com>
2022-09-06 21:59:02 +0900

    Modify : LostFound Comment ViewModel 구현
    2022-09-06

--- presentation/src/main/java/kr/hs/dgsw/smartschool/dodamdodam/features/lostfound/LostFoundCommentViewModel.kt
+++ presentation/src/main/java/kr/hs/dgsw/smartschool/dodamdodam/features/lostfound/LostFoundCommentViewModel.kt
@@ -1,7 +1,11 @@
 package kr.hs.dgsw.smartschool.dodamdodam.features.lostfound
 
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.launchIn
 import kr.hs.dgsw.smartschool.dodamdodam.base.BaseViewModel
+import kr.hs.dgsw.smartschool.domain.usecase.lostfound.GetLostFoundComment
 import kr.hs.dgsw.smartschool.domain.usecase.lostfound.LostFoundUseCases
 import javax.inject.Inject
 
@@ -9,16 +13,33 @@ import javax.inject.Inject
 class LostFoundCommentViewModel @Inject constructor(
     private val useCases : LostFoundUseCases
 ) : BaseViewModel(){
-    fun getComment(){
-
+    private val isGetCommentLoading = MutableLiveData<Boolean>()
+    fun getComment(idx : Int){
+        useCases.getLostFoundComment(GetLostFoundComment.Params(lostFoundIdx = idx)).divideResult(
+            isGetCommentLoading,
+            {},
+            {}
+        ).launchIn(viewModelScope)
     }
     fun addComment(){
-
+        useCases.addLostFoundComment().divideResult(
+            isGetCommentLoading,
+            {},
+            {}
+        ).launchIn(viewModelScope)
     }
     fun modifyComment(){
-
+        useCases.modifyLostFoundComment().divideResult(
+            isGetCommentLoading,
+            {},
+            {}
+        ).launchIn(viewModelScope)
     }
     fun deleteComment(){
-
+        useCases.deleteLostFoundComment().divideResult(
+            isGetCommentLoading,
+            {},
+            {}
+        ).launchIn(viewModelScope)
     }
 }
