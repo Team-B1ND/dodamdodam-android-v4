4b3fe26e2c8042f36b50c89ca8a1fd5546b605b6
김도현 <kdh200533@gmail.com>
2022-09-13 16:51:43 +0900

    Add : 분실물 작성 구현
    2022-09-13

--- presentation/src/main/java/kr/hs/dgsw/smartschool/dodamdodam/features/lostfound/write/LostFoundWriteViewModel.kt
+++ presentation/src/main/java/kr/hs/dgsw/smartschool/dodamdodam/features/lostfound/write/LostFoundWriteViewModel.kt
@@ -1,8 +1,11 @@
 package kr.hs.dgsw.smartschool.dodamdodam.features.lostfound.write
 
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.launchIn
 import kr.hs.dgsw.smartschool.dodamdodam.base.BaseViewModel
+import kr.hs.dgsw.smartschool.domain.request.lostfound.LostFoundDataRequest
 import kr.hs.dgsw.smartschool.domain.usecase.lostfound.LostFoundUseCases
 import javax.inject.Inject
 
@@ -10,9 +13,54 @@ import javax.inject.Inject
 class LostFoundWriteViewModel @Inject constructor(
     private val useCases : LostFoundUseCases
 ) : BaseViewModel(){
-    val isLost = MutableLiveData<Boolean>()
+    val isLost = MutableLiveData<Boolean>(true)
     val isFound = MutableLiveData<Boolean>()
     val title = MutableLiveData<String>()
     val place = MutableLiveData<String>()
     val content = MutableLiveData<String>()
+
+    private val isGetLostFoundLoading = MutableLiveData<Boolean>()
+
+    init{
+        combineLoadingVariable(isGetLostFoundLoading)
+    }
+    companion object{
+        const val EVENT_SUCCESS = 1
+        const val EVENT_EMPTY_TITLE = 2
+        const val EVENT_EMPTY_CONTENT = 3
+        const val EVENT_ERROR = 4
+    }
+
+    fun addLostFound(picture : String){
+        if(title.value.isNullOrEmpty()) viewEvent(EVENT_EMPTY_TITLE)
+        if(content.value.isNullOrEmpty()) viewEvent(EVENT_EMPTY_CONTENT)
+        useCases.addLostFound(LostFoundDataRequest(
+            content = content.value ?: "",
+            picture = "",//TODO 사진 넣어야함
+            place = place.value ?: "",
+            title = title.value ?: "",
+            type = if (isLost.value == true) "LOST" else "FOUND",
+            lostFoundId = null
+        )).divideResult(
+            isGetLostFoundLoading,
+            {viewEvent(EVENT_SUCCESS)},
+            {viewEvent(EVENT_ERROR)}
+        ).launchIn(viewModelScope)
+    }
+    fun getLostFound(){
+        //TODO 구현
+    }
+    fun modifyLostFound(){
+        if(title.value.isNullOrEmpty()) viewEvent(EVENT_EMPTY_TITLE)
+        if(content.value.isNullOrEmpty()) viewEvent(EVENT_EMPTY_CONTENT)
+        useCases.addLostFound(LostFoundDataRequest(
+            content = content.value ?: "",
+            picture = "",//TODO 사진 넣어야함
+            place = place.value ?: "",
+            title = title.value ?: "",
+            type = if (isLost.value == true) "LOST" else "FOUND",
+            lostFoundId = null
+        ))
+    }
+
 }
