b0eb88fec7744751f8bbaa10feff606aa05407c3
최민재 <cmjcmj1230@gmail.com>
2022-10-01 20:21:10 +0900

    Modify : Study Room check item

--- presentation/src/main/java/kr/hs/dgsw/smartschool/dodamdodam/adapter/StudyRoomCheckAdapter.kt
+++ presentation/src/main/java/kr/hs/dgsw/smartschool/dodamdodam/adapter/StudyRoomCheckAdapter.kt
@@ -1,16 +1,12 @@
 package kr.hs.dgsw.smartschool.dodamdodam.adapter
 
-import android.graphics.Rect
-import android.view.View
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import kr.hs.dgsw.smartschool.dodamdodam.R
 import kr.hs.dgsw.smartschool.dodamdodam.adapter.callback.StudyRoomDiffUtilCallback
 import kr.hs.dgsw.smartschool.dodamdodam.base.BaseListAdapter
 import kr.hs.dgsw.smartschool.dodamdodam.databinding.ItemStudyRoomCheckBinding
 import kr.hs.dgsw.smartschool.dodamdodam.widget.extension.timeFormat
 import kr.hs.dgsw.smartschool.domain.model.studyroom.StudyRoom
-import java.util.Date
+import java.util.*
 
 class StudyRoomCheckAdapter(val onClickStudyRoomCard: (Int) -> Unit) : BaseListAdapter<StudyRoom, ItemStudyRoomCheckBinding>(
     R.layout.item_study_room_check,
@@ -29,31 +25,6 @@ class StudyRoomCheckAdapter(val onClickStudyRoomCard: (Int) -> Unit) : BaseListA
         binding.root.setOnClickListener {
             // onOpenLocationApply.value = item.timeTableIdx ?: 0
             onClickStudyRoomCard(item.timeTable!!.id)
-        }
-    }
-
-    class StudyRoomCheckDecoration : RecyclerView.ItemDecoration() {
-
-        override fun getItemOffsets(
-            outRect: Rect,
-            view: View,
-            parent: RecyclerView,
-            state: RecyclerView.State
-        ) {
-            super.getItemOffsets(outRect, view, parent, state)
-
-            val index = (view.layoutParams as GridLayoutManager.LayoutParams).spanIndex
-
-            if (index == 0 || index == 2) {
-                outRect.right = 15
-            } else {
-                outRect.left = 15
-            }
-
-            if (index == 0 || index == 1) {
-                outRect.bottom = 30
-            }
-
         }
     }
 }
