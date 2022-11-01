f0b64c17b0fac15214eac5bdee4eeb0c2ccba158
최민재 <cmjcmj1230@gmail.com>
2022-11-01 11:28:01 +0900

    Add : Exo player

--- presentation/src/main/java/kr/hs/dgsw/smartschool/dodamdodam/features/start/StartActivity.kt
+++ presentation/src/main/java/kr/hs/dgsw/smartschool/dodamdodam/features/start/StartActivity.kt
@@ -3,29 +3,44 @@ package kr.hs.dgsw.smartschool.dodamdodam.features.start
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.motion.widget.MotionLayout
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.source.ProgressiveMediaSource
+import com.google.android.exoplayer2.ui.StyledPlayerView
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.upstream.RawResourceDataSource
+import com.google.android.exoplayer2.util.Util
 import kr.hs.dgsw.smartschool.dodamdodam.R
 import kr.hs.dgsw.smartschool.dodamdodam.features.auth.login.LoginActivity
 import kr.hs.dgsw.smartschool.dodamdodam.widget.extension.startActivityWithFinishAll
 
 class StartActivity : AppCompatActivity() {
 
-    val btnStart: Button by lazy {
+    private val btnStart: Button by lazy {
         findViewById(R.id.btn_start)
     }
 
-    val motionLayoutGatherPlatform: MotionLayout by lazy {
-        findViewById(R.id.motion_layout_gather_platform)
+    private val playerView: StyledPlayerView by lazy {
+        findViewById(R.id.player_view)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
-        motionLayoutGatherPlatform.transitionToStart()
-        motionLayoutGatherPlatform.transitionToEnd()
+
+        val exoPlayer = ExoPlayer.Builder(this).build()
+        playerView.player = exoPlayer
+
+        val uri = RawResourceDataSource.buildRawResourceUri(R.raw.dodam_android_video)
+        val mediaItem = MediaItem.fromUri(uri)
+
+        exoPlayer.setMediaItem(mediaItem)
+
+        exoPlayer.prepare()
+        exoPlayer.play()
+
 
         btnStart.setOnClickListener {
-
             startActivityWithFinishAll(LoginActivity::class.java)
         }
     }
