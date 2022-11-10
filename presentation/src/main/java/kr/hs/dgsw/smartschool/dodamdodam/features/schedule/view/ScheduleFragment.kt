7d9dd7037bd6a5845c58833e7fa77eab23b6e769
최민재 <cmjcmj1230@gmail.com>
2022-11-10 23:40:44 +0900

    Add : Basic calendar

--- presentation/src/main/java/kr/hs/dgsw/smartschool/dodamdodam/features/schedule/view/ScheduleFragment.kt
+++ presentation/src/main/java/kr/hs/dgsw/smartschool/dodamdodam/features/schedule/view/ScheduleFragment.kt
@@ -1,14 +1,18 @@
 package kr.hs.dgsw.smartschool.dodamdodam.features.schedule.view
 
+import android.graphics.Color
 import android.view.View
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.kizitonwose.calendar.core.CalendarDay
+import com.kizitonwose.calendar.core.DayPosition
+import com.kizitonwose.calendar.core.firstDayOfWeekFromLocale
 import com.kizitonwose.calendar.view.MonthDayBinder
 import kr.hs.dgsw.smartschool.dodamdodam.base.BaseFragment
 import kr.hs.dgsw.smartschool.dodamdodam.databinding.FragmentScheduleBinding
 import kr.hs.dgsw.smartschool.dodamdodam.features.schedule.container.DayViewContainer
 import kr.hs.dgsw.smartschool.dodamdodam.features.schedule.viewmodel.ScheduleViewModel
+import java.time.YearMonth
 
 class ScheduleFragment : BaseFragment<FragmentScheduleBinding, ScheduleViewModel>() {
     override val viewModel: ScheduleViewModel by viewModels()
@@ -22,14 +26,24 @@ class ScheduleFragment : BaseFragment<FragmentScheduleBinding, ScheduleViewModel
         }
 
         mBinding.calendarView.dayBinder = object : MonthDayBinder<DayViewContainer> {
-            // Called only when a new container is needed.
             override fun create(view: View) = DayViewContainer(view)
 
-            // Called every time we need to reuse a container.
             override fun bind(container: DayViewContainer, data: CalendarDay) {
                 container.textView.text = data.date.dayOfMonth.toString()
+                if (data.position == DayPosition.MonthDate) {
+                    container.textView.setTextColor(Color.BLACK)
+                } else {
+                    container.textView.setTextColor(Color.GRAY)
+                }
             }
         }
+
+        val currentMonth = YearMonth.now()
+        val startMonth = currentMonth.minusMonths(100)
+        val endMonth = currentMonth.plusMonths(100)
+        val firstDayOfWeek = firstDayOfWeekFromLocale()
+        mBinding.calendarView.setup(startMonth, endMonth, firstDayOfWeek)
+        mBinding.calendarView.scrollToMonth(currentMonth)
     }
 
 }
