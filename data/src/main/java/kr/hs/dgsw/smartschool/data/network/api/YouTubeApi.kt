084fa41e54e83c2bd7fdfd59f80985f5d72e1c16
최민재 <cmjcmj1230@gmail.com>
2023-01-23 19:15:17 +0900

    Modify : Constant to private

--- data/src/main/java/kr/hs/dgsw/smartschool/data/network/api/YouTubeApi.kt
+++ data/src/main/java/kr/hs/dgsw/smartschool/data/network/api/YouTubeApi.kt
@@ -2,6 +2,7 @@ package kr.hs.dgsw.smartschool.data.network.api
 
 import kr.hs.dgsw.smartschool.data.network.url.DodamUrl
 import kr.hs.dgsw.smartschool.data.util.Constants
+import kr.hs.dgsw.smartschool.data.util.Key
 import kr.hs.dgsw.smartschool.domain.model.song.youtube.YoutubeVideo
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -12,7 +13,7 @@ interface YouTubeApi {
     suspend fun getYouTubeVideo(
         @Query("part") part: String = "snippet",
         @Query("maxResults") maxResults: Int,
-        @Query("key") key: String = Constants.YOUTUBE_API_KEY,
+        @Query("key") key: String = Key.YOUTUBE_API_KEY,
         @Query("type") type: String = "video",
         @Query("q") content: String
     ): YoutubeVideo
