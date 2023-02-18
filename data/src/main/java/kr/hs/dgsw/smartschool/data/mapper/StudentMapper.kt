594ca8a2b1875870b488b9411d1b953b3097dcc2
윤석규 <devSxxkgyu@gmail.com>
2023-02-18 16:50:29 +0900

    Modify : StudentMapper

--- data/src/main/java/kr/hs/dgsw/smartschool/data/mapper/StudentMapper.kt
+++ data/src/main/java/kr/hs/dgsw/smartschool/data/mapper/StudentMapper.kt
@@ -1,52 +1,44 @@
 package kr.hs.dgsw.smartschool.data.mapper
 
-import kr.hs.dgsw.smartschool.data.base.BaseEntityMapper
 import kr.hs.dgsw.smartschool.data.database.entity.StudentEntity
 import kr.hs.dgsw.smartschool.domain.model.classroom.Classroom
 import kr.hs.dgsw.smartschool.domain.model.member.Member
 import kr.hs.dgsw.smartschool.domain.model.member.Student
 
-class StudentMapper : BaseEntityMapper<Student, StudentEntity> {
+fun StudentEntity.toModel(): Student = Student(
+    classroom = Classroom(
+        grade = this.grade,
+        id = this.id,
+        placeId = this.placeId,
+        room = this.room,
+    ),
+    studentId = this.studentId,
+    member = Member(
+        this.email,
+        this.memberId,
+        this.joinDate,
+        this.name,
+        this.profileImage,
+        this.role,
+        this.status
+    ),
+    number = this.number,
+    phone = this.phone
+)
 
-    override fun mapToModel(entity: StudentEntity): Student {
-        return Student(
-            Classroom(
-                grade = entity.grade,
-                id = entity.id,
-                placeId = entity.placeId,
-                room = entity.room,
-            ),
-            entity.studentId,
-            Member(
-                entity.email,
-                entity.memberId,
-                entity.joinDate,
-                entity.name,
-                entity.profileImage,
-                entity.role,
-                entity.status
-            ),
-            entity.number,
-            entity.phone
-        )
-    }
-
-    override fun mapToEntity(model: Student): StudentEntity {
-        return StudentEntity(
-            model.studentId,
-            model.classroom.grade,
-            model.classroom.id,
-            model.classroom.placeId,
-            model.classroom.grade,
-            model.number,
-            model.phone,
-            model.member.email,
-            model.member.id,
-            model.member.joinDate ?: "",
-            model.member.name,
-            model.member.profileImage ?: "",
-            model.member.role,
-            model.member.status
-        )
-    }
-}
+fun Student.toEntity(): StudentEntity = StudentEntity(
+    studentId = this.studentId,
+    grade = this.classroom.grade,
+    id = this.classroom.id,
+    placeId = this.classroom.placeId,
+    room = this.classroom.grade,
+    number = this.number,
+    phone = this.phone,
+    email = this.member.email,
+    memberId = this.member.id,
+    joinDate = this.member.joinDate ?: "",
+    name = this.member.name,
+    profileImage = this.member.profileImage ?: "",
+    role = this.member.role,
+    status = this.member.status
+)
