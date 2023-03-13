25abf9eae90012937b13f99d10e9b91c6e23d3f9
윤석규 <devSxxkgyu@gmail.com>
2023-03-13 18:51:13 +0900

    Hotfix : meal calorie error

--- data/src/main/java/kr/hs/dgsw/smartschool/data/repository/MealRepositoryImpl.kt
+++ data/src/main/java/kr/hs/dgsw/smartschool/data/repository/MealRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package kr.hs.dgsw.smartschool.data.repository
 
 import kr.hs.dgsw.smartschool.data.datasource.MealDataSource
+import kr.hs.dgsw.smartschool.domain.model.meal.Calorie
 import kr.hs.dgsw.smartschool.domain.model.meal.Meal
 import kr.hs.dgsw.smartschool.domain.repository.MealRepository
 import javax.inject.Inject
@@ -12,7 +13,12 @@ class MealRepositoryImpl @Inject constructor(
     override suspend fun getMeal(year: Int, month: Int, day: Int): Meal =
         mealDataSource.getMeal("$year-$month-$day")
 
-    override suspend fun getCalorieOfMeal(): String? {
-        return mealDataSource.getCalorieOfMeal()
+    override suspend fun getCalorieOfMeal(): String {
+        val calorie = mealDataSource.getCalorieOfMeal()
+        val breakfast = calorie.breakfast?.split(' ')?.first()?.toDouble() ?: 0.0
+        val lunch = calorie.lunch?.split(' ')?.first()?.toDouble() ?: 0.0
+        val dinner = calorie.dinner?.split(' ')?.first()?.toDouble() ?: 0.0
+
+        return "${breakfast + lunch + dinner}"
     }
 }
