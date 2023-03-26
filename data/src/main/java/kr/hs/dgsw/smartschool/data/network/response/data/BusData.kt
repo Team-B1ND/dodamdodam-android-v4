686322d1bfbb04d10039b23515880031cd53e3e9
wsi1212 <wsiwsiwsi123@gmail.com>
2023-03-26 18:28:43 +0900

    Delete: BusData, MealData
    3/26

--- data/src/main/java/kr/hs/dgsw/smartschool/data/network/response/data/BusData.kt
+++
@@ -1,8 +0,0 @@
-package kr.hs.dgsw.smartschool.data.network.response.data
-
-import com.google.gson.annotations.SerializedName
-
-data class BusData<BusType>(
-    @SerializedName("busList")
-    val busList: List<BusType>
-)
